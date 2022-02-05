ab8bd3bb2faafc973b9ce8832a87c1ce3d8453d2
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-02-05 15:57:04 +0530

    Added colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+var scaffoldColor = Color(0xff060606);
+var accentColor = Color(0xff1240A9);
+var primaryTextColor = Colors.white;
+var textFieldColor = Color(0xff1240A9);
+var gradientStartColor = Color(0xff4303CB);
+var gradientEndColor = Color(0xff00C2FF);
+var colorOfLineInCard = Color(0xffDCC1FF);
+var inactiveCardColor = Color(0xff3F435A);
+var dropDownCardColor = Color(0xff434049);
+var firstPlaceColor = Color(0xffF3BD28);
+var secondPlaceColor = Color(0xffC0C0C0);
+var thirdPlaceColor = Color(0xffCD7F32);
+var anyOtherPlaceColor = Color(0xff585651);
+var bottomBarActiveIconColor = Color(0xff6D7283);
