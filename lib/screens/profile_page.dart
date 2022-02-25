4c5689e961062bf941fb03d7bc57fbb7401643b7
sooryasrajan <soorya.s27@gmail.com>
2022-02-25 12:37:02 +0530

    Added expanded and padding to buttons in profile_page.dart

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -73,16 +73,19 @@ class ProfilePage extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 10),
                             child: Text("@" + data.data![4],
                                 style: GoogleFonts.nunito(
-                                    fontSize: 20, color: Colors.white70, fontWeight: FontWeight.bold)),
+                                    fontSize: 20,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold)),
                           ),
                           Padding(
                             padding: const EdgeInsets.only(top: 30),
                             child: Align(
                               alignment: Alignment.topLeft,
                               child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
                                 children: [
-                                  Icon(Icons.email),
+                                  const Icon(Icons.email),
                                   Text(data.data![1],
                                       style: GoogleFonts.nunito(
                                           fontSize: 17,
@@ -118,62 +121,85 @@ class ProfilePage extends StatelessWidget {
                                     child: Text(
                                       "SIGN OUT",
                                       style: GoogleFonts.nunito(
-                                          fontSize: 16, color: colors.buttonColor, fontWeight: FontWeight.bold),
+                                          fontSize: 16,
+                                          color: colors.buttonColor,
+                                          fontWeight: FontWeight.bold),
                                     )),
                               ),
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.only(top: 10),
+                            padding: const EdgeInsets.only(top: 3),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    onPrimary: colors.buttonColor,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                            const ChangeName()));
-                                  },
-                                  child: Text(
-                                        "CHANGE PASSWORD",
-                                        style: GoogleFonts.nunito(
-                                            fontSize: 14, color: Colors.white, fontWeight: FontWeight.bold),
-                                      )),
-                                ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Colors.red,
-                                      onPrimary: colors.buttonColor,
-                                    ),
-                                    onPressed: () {
-                                      displayDialog(context, "Yes", "No", () async {
-                                        var res = await makePostRequest(
-                                            null, "/deleteSelf", null, true,
-                                            context: context);
-                                        if (res.statusCode == 200) {
-                                          Navigator.of(context).pushAndRemoveUntil(
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 4),
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          onPrimary: colors.buttonColor,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).push(
                                               MaterialPageRoute(
                                                   builder: (context) =>
-                                                      const WelcomePage()),
-                                              (Route<dynamic> route) => false);
-                                          clearAllData();
-                                        } else {
-                                          showToast("Something went wrong, could not delete account!");
-                                        }
-                                      }, "Are you sure you want to delete your account?",
-                                          "Your account will be deleted and all data will be lost");
-                                    },
-                                    child: Text(
-                                          "DELETE ACCOUNT",
+                                                      const ChangeName()));
+                                        },
+                                        child: Text(
+                                          "CHANGE PASSWORD",
+                                          textAlign: TextAlign.center,
                                           style: GoogleFonts.nunito(
-                                              fontSize: 14, color: Colors.white, fontWeight: FontWeight.bold),
+                                              fontSize: 12,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
                                         )),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 4),
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          primary: Colors.red,
+                                          onPrimary: colors.buttonColor,
+                                        ),
+                                        onPressed: () {
+                                          displayDialog(context, "Yes", "No",
+                                              () async {
+                                            var res = await makePostRequest(
+                                                null, "/deleteSelf", null, true,
+                                                context: context);
+                                            if (res.statusCode == 200) {
+                                              Navigator.of(
+                                                      context)
+                                                  .pushAndRemoveUntil(
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              const WelcomePage()),
+                                                      (Route<dynamic> route) =>
+                                                          false);
+                                              clearAllData();
+                                            } else {
+                                              showToast(
+                                                  "Something went wrong, could not delete account!");
+                                            }
+                                          }, "Are you sure you want to delete your account?",
+                                              "Your account will be deleted and all data will be lost");
+                                        },
+                                        child: Text(
+                                          "DELETE ACCOUNT",
+                                          textAlign: TextAlign.center,
+                                          style: GoogleFonts.nunito(
+                                              fontSize: 12,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                  ),
+                                ),
                               ],
                             ),
-                            ),
+                          ),
                         ],
                       ),
                     ));
